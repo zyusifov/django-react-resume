a373301dd1edebe3c5980ab9b9fe94f14dd77a4a
Zaman Yusifov <zamanyusifov2002@gmail.com>
2022-04-02 16:29:08 +0400

    Register and Login by jwt token

--- backend/apps/account/urls.py
+++ backend/apps/account/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from .views import MyObtainTokenPairView, RegisterView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
-    
+    path('signup/', RegisterView.as_view(), name='register'),
+
+    path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
