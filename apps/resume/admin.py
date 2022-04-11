7f0addf297bcefc4dbb770703e0ae511ba6c790e
Zaman Yusifov <zamanyusifov2002@gmail.com>
2022-04-12 00:32:15 +0400

    first resume api

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(Resume)
+admin.site.register(Skill)
+admin.site.register(Education)
--- apps/resume/urls.py
+++ apps/resume/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('', ResumeListView.as_view()),
+    path('<int:pk>/', ResumeView.as_view()),
+    path('create/', ResumeCreatView.as_view())
 ]
