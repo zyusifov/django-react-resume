886314f0b3a19b21177c5b1694c17631d0dba08a
Zaman Yusifov <zamanyusifov2002@gmail.com>
2022-04-17 23:48:36 +0400

    resume serializer

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -5,3 +5,4 @@ from .models import *
 admin.site.register(Resume)
 admin.site.register(Skill)
 admin.site.register(Education)
+admin.site.register(WorkExperience)
--- apps/resume/serializer.py
+++ apps/resume/serializer.py
@@ -1,16 +1,42 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Resume
+from rest_framework.serializers import ModelSerializer, HyperlinkedRelatedField, HyperlinkedModelSerializer
 
+from apps.account.models import User
+from .models import Resume, Skill, WorkExperience
+
+
+class SkillSerializer(ModelSerializer):
+
+    class Meta:
+        model = Skill
+        fields = ('name',)
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'about', 'phone_no',)
+
+
+class WorkExperienceSerializer(ModelSerializer):
+
+    class Meta:
+        model = WorkExperience
+        fields = ('name',)
 
 class ResumeSerializer(ModelSerializer):
     
+    skill = SkillSerializer(many=True)
+    user = UserSerializer()
+    work_experience = WorkExperienceSerializer(many=True)
+
     class Meta:
         model = Resume
-        fields = '__all__'
+        fields = ('id', 'user', 'skill', 'work_experience', 'work_experience_time',)
 
 
 class UserResumeSerializer(ModelSerializer):
     
     class Meta:
         model = Resume
-        exclude = ('user', )
+        exclude = ('user',)
