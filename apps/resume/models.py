cbd255931a678a87d0690255a33f0847bf552fb4
Zaman Yusifov <zamanyusifov2002@gmail.com>
2022-04-20 00:37:09 +0400

    more, more and more new updates

--- apps/resume/models.py
+++ apps/resume/models.py
@@ -10,13 +10,18 @@ class Education(models.Model):
     name = models.TextField(verbose_name='Education name', unique=True)
 
 
+class Language(models.Model):
+    name = models.TextField(unique=True)
+
+
 class WorkExperience(models.Model):
     name = models.TextField(verbose_name='Work name', unique=True)
 
 
 class Resume(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    skill = models.ManyToManyField(Skill, null=True)
-    education = models.ManyToManyField(Education, null=True)
-    work_experience = models.ManyToManyField(WorkExperience, null=True)
+    skill = models.ManyToManyField(Skill)
+    education = models.ManyToManyField(Education)
+    work_experience = models.ManyToManyField(WorkExperience)
     work_experience_time = models.FloatField(null=True)
+    language = models.ManyToManyField(Language)
